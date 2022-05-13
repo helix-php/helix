ec38846103196b4e73756f895f94a66c7d879afa
SerafimArts <nesk@xakep.ru>
2022-05-14 00:03:21 +0300

    Create routes php list

--- app/Extension/RouterExtension.php
+++ app/Extension/RouterExtension.php
@@ -15,7 +15,7 @@ use Helix\Boot\Attribute\Singleton;
 use Helix\Contracts\Router\RegistrarInterface;
 use Helix\Contracts\Router\RepositoryInterface;
 use Helix\Contracts\Router\RouterInterface;
-use Helix\Router\Exception\BadRouteDefinitionException;
+use Helix\Foundation\Path;
 use Helix\Router\Generator\Generator;
 use Helix\Router\Generator\GeneratorInterface;
 use Helix\Router\Router;
@@ -25,15 +25,18 @@ use Psr\Http\Message\UriFactoryInterface;
 final class RouterExtension
 {
     /**
+     * @param Path $path
      * @param ResponseFactoryInterface $resp
      * @param UriFactoryInterface $uris
      * @return RouterInterface
-     * @throws BadRouteDefinitionException
      */
     #[Singleton(as: [Router::class, RegistrarInterface::class, RepositoryInterface::class])]
-    public function getRouter(ResponseFactoryInterface $resp, UriFactoryInterface $uris): RouterInterface
+    public function getRouter(Path $path, ResponseFactoryInterface $resp, UriFactoryInterface $uris): RouterInterface
     {
-        $this->routes($router = new Router($resp, $uris));
+        $router = new Router($resp, $uris);
+
+        $registrar = require $path->app('routes.php');
+        $registrar($router);
 
         return $router;
     }
@@ -47,14 +50,5 @@ final class RouterExtension
     public function getRouteGenerator(UriFactoryInterface $uris, RepositoryInterface $router): GeneratorInterface
     {
         return new Generator($uris, $router);
-    }
-    /**
-     * @param Router $router
-     * @return void
-     * @throws BadRouteDefinitionException
-     */
-    private function routes(Router $router): void
-    {
-        $router->import(\App\Http\Controller\HomeController::class);
     }
 }
---
+++ app/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of Helix package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use Helix\Router\Router;
+
+return static function (Router $router): void {
+
+    $router->import(\App\Http\Controller\HomeController::class);
+
+};
